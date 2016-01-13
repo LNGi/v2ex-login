a83838b58da8b8e78035d01cd7089ce05a3bc920
micookie <hdjdcyl@126.com>
2016-01-13 21:33:07 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,15 +1,15 @@
 <?php
 
-	$v2ex = new V2EX();
-	//登陆
+    $v2ex = new V2EX();
+    //登陆
     $v2ex->login('账号','密码');
-		//需要代理的配置代理
-		//$v2ex->setProxy("127.0.0.1", 8888)->login('账号','密码');
-	//执行签到
+	//需要代理的配置代理
+	//$v2ex->setProxy("127.0.0.1", 8888)->login('账号','密码');
+    //执行签到
     $v2ex->signin();
 
 
-	function __autoload($className){  //自动引入类库
-	    include './class/'.$className.'.class.php';
+    function __autoload($className){  //自动引入类库
+        include './class/'.$className.'.class.php';
     }
 ?>
